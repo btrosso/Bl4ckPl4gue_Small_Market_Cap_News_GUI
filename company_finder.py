5812f7e98ca5a91301c7aac9c5d6d8dff5c7252f
James Chua <chua.yijin35@hotmail.com>
2022-03-05 11:15:09 +0800

    Added a stock screening function in CompanyFinder class

--- company_finder.py
+++ company_finder.py
@@ -5,16 +5,34 @@ from dotenv import load_dotenv
 load_dotenv()
 
 ALPHAV_ENDPOINT = "https://www.alphavantage.co/query"
+FMP_ENDPOINT = "https://financialmodelingprep.com/api/v3"
 
 
 class CompanyFinder:
     def __init__(self):
         pass
 
-    def get_company_data(self, company):
+    def stock_screener(self):
+        ticker_list = []
+        screening_criteria = {
+            "marketCapMoreThan": 1000000000,
+            "marketCapLowerThan": 3000000000,
+            "volumeMoreThan": 1000000,
+            "exchange": "nyse,nasdaq",
+            "apikey": os.environ["FMP_API_KEY"]
+        }
+        tickers_json = requests.get(
+            url=f"{FMP_ENDPOINT}/stock-screener",
+            params=screening_criteria
+        ).json()
+        for ticker in tickers_json:
+            ticker_list.append(ticker["symbol"])
+        return ticker_list
+
+    def get_company_data(self, ticker):
         query_params = {
             "function": "OVERVIEW",
-            "symbol": company,
+            "symbol": ticker,
             "apikey": os.environ["ALPHAV_API_KEY"],
         }
         overview = requests.get(url=f"{ALPHAV_ENDPOINT}", params=query_params).json()
--- main.py
+++ main.py
@@ -2,13 +2,11 @@ from user_interface import UserInterface
 from company_finder import CompanyFinder
 
 company_finder = CompanyFinder()
-
-# app = UserInterface()
-
-company_list = ["AAPL"]
+app = UserInterface()
 
 # TODO: DECIDE HOW TO DO THE API STUFF OOP STYLE
 
-for company in company_list:
-    company_data = company_finder.get_company_data(company)
-    print(company_data.market_cap)
+ticker_list = company_finder.stock_screener()
+# for ticker in ticker_list:
+#     company_data = company_finder.get_company_data(ticker)
+#     print(company_data.market_cap)
